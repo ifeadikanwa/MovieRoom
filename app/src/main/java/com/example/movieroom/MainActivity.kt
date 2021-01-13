12e19adebdd53fc80bdf11f66a016fd314959fdd
ifeadikanwa <ezeifyyy@gmail.com>
2021-01-12 19:31:03 -0500

    setup bottom navigation bar

--- app/src/main/java/com/example/movieroom/MainActivity.kt
+++ app/src/main/java/com/example/movieroom/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.movieroom
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.movieroom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //setup databinding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //nav controller
+        val navController = this.findNavController(R.id.navHostFragment)
+
+        //setup bottom navigation bar
+        NavigationUI.setupWithNavController(binding.bottomNavBar, navController )
     }
 }
